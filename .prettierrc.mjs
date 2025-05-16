88bbd04eb6b51eebedf6439a61037db65595058e
Webber <webber@takken.io>
2025-05-16 18:33:42 +0200

    chore: formatting

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,17 +1,14 @@
 /** @type {import("prettier").Config} */
 export default {
   plugins: ["prettier-plugin-astro"],
-  options: {
-    printWidth: 120,
-    proseWrap: "always",
-    astroSkipFrontmatter: false,
-  },
   overrides: [
     {
       files: "*.astro",
       options: {
         parser: "astro",
-        astroSkipFrontmatter: false,
+        printWidth: 120,
+        proseWrap: "always",
+        astroSkipFrontmatter: true,
       },
     },
   ],
